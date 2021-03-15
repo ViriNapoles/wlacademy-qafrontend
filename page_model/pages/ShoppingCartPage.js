cf1e6cbf036ad5a53a645395a7e24fed6182f888
Viri Napoles <viri_napoles@hotmail.com>
2021-03-15 00:52:36 -0600

    refactoring class selector

--- page_model/pages/ShoppingCartPage.js
+++ page_model/pages/ShoppingCartPage.js
@@ -5,7 +5,7 @@ class ShoppingCart {
         this.pageTitle = Selector ('#contents_wrapper .subheader').withText('Your Cart')
         this.shoppingCartButton = Selector ('.shopping_cart_container .shopping_cart_link.fa-layers.fa-fw')
         this.continueShoppingButton = Selector ('.cart_contents_container .btn_secondary')
-        this.checkoutButton = Selector('.cart_contents_container div[class="btn_action checkout_button"]')
+        this.checkoutButton = Selector('.cart_contents_container .btn_action checkout_button')
     }
 
     async navigateToShoppingCart(){
