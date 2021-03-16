e986220200e23c34f18cbf8ae9cefd671322d766
Viri Napoles <viri_napoles@hotmail.com>
2021-03-15 18:37:52 -0600

    remove unused code

--- page_model/pages/ProductsPage.js
+++ page_model/pages/ProductsPage.js
@@ -1,5 +1,4 @@
 import { Selector, t } from 'testcafe'
-import ShoppingCart from '../pages/ShoppingCartPage'
 
 export const ADD_LABEL = 'ADD TO CART'
 export const REMOVE_LABEL = 'REMOVE'
@@ -10,11 +9,7 @@ class ProductsPage {
         this.pageTitle = Selector('#inventory_filter_container div[class="product_label"]')
         this.burgerButton = Selector('#react-burger-menu-btn')
         this.burgerLogout = Selector('.bm-menu-wrap a[id="logout_sidebar_link"]').withText('Logout')
-        //this.itemButton01 = Selector('.inventory_item .pricebar .btn_inventory').nth(0)
-        //this.itemButton02 = Selector('.inventory_item .pricebar .btn_inventory').nth(1)
-        //this.itemButton03 = Selector('.inventory_item .pricebar .btn_inventory').nth(2)
-        this.itemButton = Selector('.inventory_item .pricebar .btn_inventory')
-        
+        this.itemButton = Selector('.inventory_item .pricebar .btn_inventory')     
     }
 
     async logoutUser() {
--- page_model/test/AddingItems.test.js
+++ page_model/test/AddingItems.test.js
@@ -1,7 +1,7 @@
 import LoginPage from '../pages/LoginPage'
 import CREDENTIALS from '../data/Constants'
-import ProductsPage, {ADD_LABEL, REMOVE_LABEL} from '../pages/ProductsPage'
-import ShoppingCart, { NAME_ITEM01, NAME_ITEM02, NAME_ITEM03 } from '../pages/ShoppingCartPage'
+import ProductsPage from '../pages/ProductsPage'
+import ShoppingCart from '../pages/ShoppingCartPage'
 
 fixture('Adding items to the shopping cart')
     .page`https://www.saucedemo.com/`
@@ -13,13 +13,6 @@ test('Add a single item to the shopping cart', async t=>{
     await t.expect(ProductsPage.pageTitle.exists).ok()
     await ProductsPage.addItem(0)
     await ShoppingCart.validateItem(0)
-
-        // .expect(ProductsPage.itemButton01.innerText).contains(ADD_LABEL)
-        // .click(ProductsPage.itemButton01)
-        //.expect(ProductsPage.itemButton01.innerText).contains(REMOVE_LABEL)
-        // .expect(ShoppingCart.shoppingCarteBadge.exists).ok()
-        // .navigateTo('https://www.saucedemo.com/cart.html')
-        // .expect(ShoppingCart.itemName01.innerText).contains(NAME_ITEM01)
 })
 
 test('Add multiple items to the shopping cart', async t =>{
@@ -30,17 +23,4 @@ test('Add multiple items to the shopping cart', async t =>{
     await ShoppingCart.validateItem(0)
     await ShoppingCart.validateItem(1)
     await ShoppingCart.validateItem(2)
-    //await t
-        // .expect(ProductsPage.pageTitle.exists).ok()
-        // .expect(ProductsPage.itemButton01.innerText).contains(ADD_LABEL)
-        // .click(ProductsPage.itemButton01)
-        // .expect(ProductsPage.itemButton02.innerText).contains(ADD_LABEL)
-        // .click(ProductsPage.itemButton02)
-        // .expect(ProductsPage.itemButton03.innerText).contains(ADD_LABEL)
-        // .click(ProductsPage.itemButton03)
-        // .expect(ShoppingCart.shoppingCarteBadge.innerText).contains('3')
-        // .navigateTo('https://www.saucedemo.com/cart.html')
-        // .expect(ShoppingCart.itemName01.innerText).contains(NAME_ITEM01)
-        // .expect(ShoppingCart.itemName02.innerText).contains(NAME_ITEM02)
-        // .expect(ShoppingCart.itemName03.innerText).contains(NAME_ITEM03)
 })
