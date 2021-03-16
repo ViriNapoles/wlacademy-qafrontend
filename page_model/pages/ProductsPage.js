1771c7940c4b3cdf79d69c833af2f244ed66995b
Viri Napoles <viri_napoles@hotmail.com>
2021-03-15 18:32:49 -0600

    refactoring functions

--- page_model/pages/ProductsPage.js
+++ page_model/pages/ProductsPage.js
@@ -1,4 +1,5 @@
 import { Selector, t } from 'testcafe'
+import ShoppingCart from '../pages/ShoppingCartPage'
 
 export const ADD_LABEL = 'ADD TO CART'
 export const REMOVE_LABEL = 'REMOVE'
@@ -9,9 +10,10 @@ class ProductsPage {
         this.pageTitle = Selector('#inventory_filter_container div[class="product_label"]')
         this.burgerButton = Selector('#react-burger-menu-btn')
         this.burgerLogout = Selector('.bm-menu-wrap a[id="logout_sidebar_link"]').withText('Logout')
-        this.itemButton01 = Selector('.inventory_item .pricebar .btn_inventory').nth(0)
-        this.itemButton02 = Selector('.inventory_item .pricebar .btn_inventory').nth(1)
-        this.itemButton03 = Selector('.inventory_item .pricebar .btn_inventory').nth(2)
+        //this.itemButton01 = Selector('.inventory_item .pricebar .btn_inventory').nth(0)
+        //this.itemButton02 = Selector('.inventory_item .pricebar .btn_inventory').nth(1)
+        //this.itemButton03 = Selector('.inventory_item .pricebar .btn_inventory').nth(2)
+        this.itemButton = Selector('.inventory_item .pricebar .btn_inventory')
         
     }
 
@@ -19,6 +21,13 @@ class ProductsPage {
         await t.expect(this.pageTitle.exists).ok()
         await t.click(this.burgerButton)
         await t.click(this.burgerLogout)
+    }
+
+    async addItem(itemPosition) {
+        await t
+            .expect(this.itemButton.nth(itemPosition).innerText).contains(ADD_LABEL)
+            .click(this.itemButton.nth(itemPosition))
+            .expect(this.itemButton.nth(itemPosition).innerText).contains(REMOVE_LABEL)
     }
 }
 
--- page_model/pages/ShoppingCartPage.js
+++ page_model/pages/ShoppingCartPage.js
@@ -1,8 +1,8 @@
 import {Selector, t} from 'testcafe'
 
-export const NAME_ITEM01 = 'Sauce Labs Backpack'
-export const NAME_ITEM02 = 'Sauce Labs Bike Light'
-export const NAME_ITEM03 = 'Sauce Labs Bolt T-Shirt'
+//export const NAME_ITEM01 = 'Sauce Labs Backpack'
+//export const NAME_ITEM02 = 'Sauce Labs Bike Light'
+//export const NAME_ITEM03 = 'Sauce Labs Bolt T-Shirt'
 
 class ShoppingCart {
     constructor(){
@@ -11,14 +11,25 @@ class ShoppingCart {
         this.continueShoppingButton = Selector ('.cart_contents_container .btn_secondary')
         this.checkoutButton = Selector('.cart_contents_container .btn_action checkout_button')
         this.shoppingCarteBadge = Selector('.shopping_cart_container .shopping_cart_link.fa-layers.fa-fw .fa-layers-counter.shopping_cart_badge')
-        this.itemName01 = Selector ('.cart_list .cart_item .inventory_item_name').nth(0)
-        this.itemName02 = Selector ('.cart_list .cart_item .inventory_item_name').nth(1)
-        this.itemName03 = Selector ('.cart_list .cart_item .inventory_item_name').nth(2)
+        //this.itemName01 = Selector ('.cart_list .cart_item .inventory_item_name').nth(0)
+        //this.itemName02 = Selector ('.cart_list .cart_item .inventory_item_name').nth(1)
+        //this.itemName03 = Selector ('.cart_list .cart_item .inventory_item_name').nth(2)
+        this.itemName = Selector ('.inventory_item_name')
+        this.cartItemName = Selector('.cart_list .cart_item .inventory_item_name')
     }
 
     async navigateToShoppingCart(){
         await t.click(this.shoppingCartButton)
     }
+
+    async validateItem(itemPosition){
+        const itemName = await this.itemName.nth(itemPosition).innerText
+        // console.log({itemName})
+        await t.navigateTo('https://www.saucedemo.com/cart.html')
+        await t.expect(this.cartItemName.nth(itemPosition).innerText).contains(itemName)
+        await t.navigateTo('https://www.saucedemo.com/inventory.html')
+    }
+
 }
 
 export default new ShoppingCart() 
